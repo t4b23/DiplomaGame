8a76a7b9d50cebdfa4a4db68a9c7e7aeeb5b1fa6
t4b23 <andreev00.00@mail.ru>
2023-11-04 14:46:53 +0300

    move

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,22 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
 public class PlayerControl : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public Transform movePoint;
+    [SerializeField]
+    private Tilemap groundTilemap;
+    [SerializeField]
+    private Tilemap collisionTilemap;
+    private Controls controler;
+
+    [SerializeField] private float walkTimer = 0.5f;
+    public Transform playerSprite;
+    public float moveSpeed;
+    public bool isMoving;
+    private Vector3 origPos;
+    private Vector3 targetPos;
 
     // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        movePoint.parent = null;
+        controler = new Controls();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-       
+        //controler.PC.Movement.performed += ctx => Move(ctx.ReadValue<Vector2>());
+        
+    }
+
+    private void Update()
+    {
+        
+        if (controler.PC.Movement.IsInProgress())
+        {
+            Move(controler.PC.Movement.ReadValue<Vector2>());
+        }
+
+    }
+
+    private void OnEnable()
+    {
+        controler.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controler.Disable();
+    }
+
+    private IEnumerator Move(Vector2 direction)
+    {
+        isMoving = true;
+            float elapsedTime = 0;
+        origPos = transform.position;
+        if (CanMove(direction))
+        {
+            targetPos = origPos + direction;
+        }
+
+        while(elapsedTime < walkTimer)
+        {
+            transform.position = Vector3.Lerp(origPos,targetPos,(elapsedTime / walkTimer));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+
+
+    private bool CanMove(Vector2 direction)
+    {
+        Vector3Int gridPosition = groundTilemap.WorldToCell(transform.position + (Vector3)direction);
+        if(!groundTilemap.HasTile(gridPosition) || collisionTilemap.HasTile(gridPosition))
+            return false;
+        return true;
     }
 }
