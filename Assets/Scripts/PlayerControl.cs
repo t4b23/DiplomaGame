1a9667ecce9c131fcf879841bf9ab0ba9205d3f1
t4b23 <andreev00.00@mail.ru>
2023-11-05 22:39:48 +0300

    camera and different movement

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,39 +8,17 @@ using UnityEngine.Tilemaps;
 
 public class PlayerControl : MonoBehaviour
 {
-    [SerializeField]
-    private Tilemap groundTilemap;
-    [SerializeField]
-    private Tilemap collisionTilemap;
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
     private Controls controler;
 
-    [SerializeField] private float walkTimer = 0.5f;
-    public Transform playerSprite;
-    public float moveSpeed;
-    public bool isMoving;
-    private Vector3 origPos;
-    private Vector3 targetPos;
+    public Vector2 movement = Vector2.zero;
 
-    // Start is called before the first frame update
     private void Awake()
     {
         controler = new Controls();
-    }
-
-    void Start()
-    {
-        //controler.PC.Movement.performed += ctx => Move(ctx.ReadValue<Vector2>());
-        
-    }
-
-    private void Update()
-    {
-        
-        if (controler.PC.Movement.IsInProgress())
-        {
-            Move(controler.PC.Movement.ReadValue<Vector2>());
-        }
-
     }
 
     private void OnEnable()
@@ -52,34 +31,82 @@ public class PlayerControl : MonoBehaviour
         controler.Disable();
     }
 
-    private IEnumerator Move(Vector2 direction)
+    void Update()
     {
-        isMoving = true;
-            float elapsedTime = 0;
-        origPos = transform.position;
-        if (CanMove(direction))
-        {
-            targetPos = origPos + direction;
-        }
-
-        while(elapsedTime < walkTimer)
-        {
-            transform.position = Vector3.Lerp(origPos,targetPos,(elapsedTime / walkTimer));
-            elapsedTime += Time.deltaTime;
-            yield return null;
-        }
-
-        transform.position = targetPos;
-
-        isMoving = false;
+            movement = controler.PC.Movement.ReadValue<Vector2>();            ;
     }
 
-
-    private bool CanMove(Vector2 direction)
-    {
-        Vector3Int gridPosition = groundTilemap.WorldToCell(transform.position + (Vector3)direction);
-        if(!groundTilemap.HasTile(gridPosition) || collisionTilemap.HasTile(gridPosition))
-            return false;
-        return true;
+    void FixedUpdate()
+    {     
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
+
 }
+
+
+
+/*[SerializeField]
+private Tilemap groundTilemap;
+[SerializeField]
+private Tilemap collisionTilemap;
+private Controls controler;
+
+[SerializeField] private float walkTimer = 0.5f;
+public Transform playerSprite;
+public float moveSpeed;
+public bool isMoving;
+private Vector3 origPos;
+private Vector3 targetPos;
+
+// Start is called before the first frame update
+private void Awake()
+{
+    controler = new Controls();
+}
+
+void Start()
+{
+    //controler.PC.Movement.performed += ctx => Move(ctx.ReadValue<Vector2>());
+
+}
+
+private void Update()
+{
+
+    if (controler.PC.Movement.IsInProgress())
+    {
+        //Debug.Log("Movement button pressed");
+        Move(controler.PC.Movement.ReadValue<Vector2>());
+    }
+
+}
+
+private void OnEnable()
+{
+    controler.Enable();
+}
+
+private void OnDisable()
+{
+    controler.Disable();
+}
+
+private void Move(Vector2 direction)
+{
+    if (CanMove(direction))
+    {
+        //Debug.Log("Can move, trying to do movement");
+        transform.position += (Vector3)direction;
+    }
+
+}
+
+
+private bool CanMove(Vector2 direction)
+{
+    Debug.Log("Checking if can move");
+    Vector3Int gridPosition = groundTilemap.WorldToCell(transform.position + (Vector3)direction);
+    if (!groundTilemap.HasTile(gridPosition) || collisionTilemap.HasTile(gridPosition))
+        return false;
+    return true;
+}*/
